5b7e421abfd585c6e1a023959b161420854effe6
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2023-11-03 18:02:01 +0700

    add:login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './components/Sidebar'
-import { ChakraProvider, Flex } from '@chakra-ui/react'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import BookList from './pages/Booklist'
-import Playlist from './pages/Playlist'
-import Subscriber from './pages/Subscriber'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import BookList from "./pages/Booklist";
+import Playlist from "./pages/Playlist";
+import Subscriber from "./pages/Subscriber";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -16,18 +17,21 @@ function App() {
       <BrowserRouter>
         <Flex>
           <Sidebar />
-          <Flex flex="1" p="20px" marginLeft="1rem"> {/* Content container */}
+          <Flex flex="1" p="20px" marginLeft="1rem">
+            {" "}
+            {/* Content container */}
             <Routes>
               <Route path="/home" element={<Home />} />
               <Route path="/books" element={<BookList />} />
               <Route path="/playlists" element={<Playlist />} />
               <Route path="/subscribers" element={<Subscriber />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </Flex>
         </Flex>
       </BrowserRouter>
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
---
+++ src/pages/login.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import backgroundImage from "../assets/logo.svg";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+
+export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.target.value);
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(event.target.value);
+
+  const validateEmailAndPassword = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^.{8,}$/;
+    let valid = true;
+    if (!emailRegex.test(email)) {
+      setEmailError("Email format is invalid");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+    if (!passwordRegex.test(password)) {
+      setPasswordError("Password must contain at least 8 characters");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+    return valid;
+  };
+
+  const handleLogin = () => {
+    if (validateEmailAndPassword()) {
+      //Implementasi Nanti
+    }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Box
+      sx={{
+        minH: "100vh",
+        minW: "100vw",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+      }}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack marginTop={"5vh"}>
+          <Heading fontSize={"4xl"}>Welcome Back 👋</Heading>
+          <Button
+            leftIcon={<FaGoogle />}
+            colorScheme="black"
+            variant="outline"
+            size="md"
+            mt={4}
+            _hover={{ bg: "white" }}
+            onClick={handleLogin}
+          >
+            Sign in with Google
+          </Button>
+          <FormControl id="email">
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={handleUsernameChange}
+              bg="white"
+              placeholder="Masukkan email"
+            />
+            {emailError && <Text color="gray">{emailError}</Text>}
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <InputGroup>
+              <Input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={handlePasswordChange}
+                bg="white"
+                placeholder="Masukkan password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleShowPassword}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            {passwordError && <Text color="gray">{passwordError}</Text>}
+          </FormControl>
+          <Button
+            colorScheme="yellow"
+            variant="solid"
+            size="md"
+            mt={4}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <Text fontSize={"md"} color={"gray.600"} mt={4}>
+            Don't have an account?{" "}
+            <Link to="/register" style={{ color: "black" }}>
+              Register
+            </Link>{" "}
+          </Text>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
